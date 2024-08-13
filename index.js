711bff3b77bfaa5d02a3f718884fd6bc14cc7643
aviranyosefi <aviran5600@gmail.com>
2024-08-13 16:40:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+debugger;
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import {getDatabase,ref,push,onValue , remove,set , query, orderByChild, equalTo , get   } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 const appSettings = {
     databaseURL: "https://shopping-ac927-default-rtdb.europe-west1.firebasedatabase.app/",   
   };
+  var lastcb='';
+
+  const  inputFieldEl = document.getElementById("input=field")
+const  addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
+const searchInput = document.getElementById('searchInput');
+//const milk_box = document.getElementById('milk-box');
+var buttons = document.getElementsByClassName('checkbox-click');
 
 const app = initializeApp(appSettings);
 const database = getDatabase(app)
@@ -23,49 +32,267 @@ onValue(shoopingListDB, function(snapshot){
 })
 
 function addList(row){
+
         let ID = row[0];
         let name = row[1].name;
         let color = row[1].color;
-
+        let category =row[1].category
+        if(category ==undefined) category = 'other'
+        else if(category =="dry") category = 'other'
+        var show =  getCbByCategory(category)
+        if(color == 'green' || color== '#7DE5E5') color= '#7DE5E5'
         let newEl = document.createElement("li");
-        newEl.textContent =name
+        newEl.textContent =name 
         newEl.id = ID;
+        newEl.style.display = show
         newEl.style.backgroundColor  = color
         let LocationInDB = ref(database,`shoopingList/${ID}`);
-       // newEl.addEventListener("dblclick",function(){  
-         //   remove(LocationInDB)
-       // });
-       
+        newEl.className = "li-" +category
+        var longClickTimer;
+        var longClickOccurred = false;
         newEl.addEventListener("click",function(){
-            debugger;
-            newEl.className = "li-white";
+          debugger;
+            if(color == 'green' ||color=='#7DE5E5' || longClickOccurred ) return;
+            newEl.classList.add('li-white');
             let changeColor= "white";
             if(color == 'white'){
-                changeColor = 'green'
-                newEl.className = "li-green";    
+                changeColor='#7DE5E5'
+                newEl.classList.add('li-green');
             }
-            //newEl.style.color = changeColor;
             set(LocationInDB, {
                 name : name,
-                color:changeColor
+                color:changeColor,
+                category:category
               });
+              searchInput.value = '';
         })
+
+        newEl.addEventListener("dblclick",function(){
+          if(color == 'white' ) return;
+          newEl.classList.add('li-white');
+          let changeColor= "white";
+          if(color == 'white'){
+              changeColor='#009aa5'
+              newEl.classList.add('li-green');
+
+          }
+          set(LocationInDB, {
+              name : name,
+              color:changeColor,
+              category:category
+            });
+            searchInput.value = '';
+      })
+
+      newEl.addEventListener("mousedown",function(){
+
+        longClickTimer = setTimeout(function() {
+
+          longClickOccurred = true;
+          //alert("Long click event detected on " + this.textContent);
+          Swal.fire({
+            title: 'מסך עדכון מוצר',
+            html:
+            `<input type="text" id="name"  placeholder="שם מוצר" class="swal2-input" value="${this.innerText}">`,
+            input: 'select',
+            inputOptions: {
+              'milk': 'מוצרי חלב',
+              'fruits': 'פירות',
+              'veg': 'ירקות',
+              "bread":"מאפים",
+              "meat":"בשר",
+              "clean": "ניקיון",
+              "other":"אחר"
+            },
+            inputValue: category,
+            showCloseButton: true,
+            showCancelButton: true,
+            cancelButtonText: 'בטל',
+            confirmButtonText: 'עדכון',
+            confirmButtonColor: 'blue',
+            showDenyButton: true,
+            denyButtonText: `מחיקת מוצר`,
+            inputValidator: (value) => {
+              return new Promise((resolve) => {
+                if (value !== '') {
+                  resolve();
+                } else {
+                  resolve('Please select an option');
+                }
+              });
+            },
+            preConfirm: () => {
+              // Modify the confirm button style programmatically
+              const confirmButton = Swal.getConfirmButton();
+              confirmButton.style.color = 'red';
+            }
+          }).then((result) => {
+            if (result.isConfirmed) {
+              const selectedOption = result.value;
+              var name = document.getElementById("name").value;
+              set(LocationInDB, {
+                name : name,
+                color:color,
+                category:selectedOption
+              }).then(function() {
+                inputFieldEl.value = '';
+                Swal.fire({
+                  position: 'top',
+                  icon: 'success',
+                  title: 'המוצר נשמר בהצלחה',
+                  showConfirmButton: false,
+                  timer: 1500
+                })
+              })
+              .catch(function(error) {
+                console.log("Error setting data:", error);
+              });                                           
+            }
+            else if (result.isDenied) {
+              remove(LocationInDB)
+              .then(() => {
+                inputFieldEl.value = '';
+              Swal.fire({
+                position: 'top',
+                icon: 'success',
+                title: 'המוצר נמחק בהצלחה',
+                showConfirmButton: false,
+                timer: 1500
+              })
+            })
+            .catch((error) => {
+                console.error('Error setting data:', error);
+            });
+              
+            }
+          });
+        }.bind(this), 1200);
+
+      });
+
+      newEl.addEventListener("mouseup",function(){
+        clearTimeout(longClickTimer);
+        longClickOccurred = false
+      });
+
+      newEl.addEventListener("touchstart",function(){
+
+        longClickTimer = setTimeout(function() {
+
+          longClickOccurred = true;
+          //alert("Long click event detected on " + this.textContent);
+          Swal.fire({
+            title: 'מסך עדכון מוצר',
+            html:
+            `<input type="text" id="name"  placeholder="שם מוצר" class="swal2-input" value="${this.innerText}">`,
+            input: 'select',
+            inputOptions: {
+              'milk': 'מוצרי חלב',
+              'fruits': 'פירות',
+              'veg': 'ירקות',
+              "bread":"מאפים",
+              "meat":"בשר",
+              "clean": "ניקיון",
+              "other":"אחר"
+            },
+            inputValue: category,
+            showCloseButton: true,
+            showCancelButton: true,
+            cancelButtonText: 'בטל',
+            confirmButtonText: 'עדכון',
+            confirmButtonColor: 'blue',
+            showDenyButton: true,
+            denyButtonText: `מחיקת מוצר`,
+            inputValidator: (value) => {
+              return new Promise((resolve) => {
+                if (value !== '') {
+                  resolve();
+                } else {
+                  resolve('Please select an option');
+                }
+              });
+            },
+            preConfirm: () => {
+              // Modify the confirm button style programmatically
+              const confirmButton = Swal.getConfirmButton();
+              confirmButton.style.color = 'red';
+            }
+          }).then((result) => {
+            if (result.isConfirmed) {
+              const selectedOption = result.value;
+              var name = document.getElementById("name").value;
+              set(LocationInDB, {
+                name : name,
+                color:color,
+                category:selectedOption
+              }).then(function() {
+                inputFieldEl.value = '';
+                Swal.fire({
+                  position: 'top',
+                  icon: 'success',
+                  title: 'המוצר נשמר בהצלחה',
+                  showConfirmButton: false,
+                  timer: 1500
+                })
+              })
+              .catch(function(error) {
+                console.log("Error setting data:", error);
+              });                                           
+            }
+            else if (result.isDenied) {
+              remove(LocationInDB)
+              .then(() => {
+                inputFieldEl.value = '';
+              Swal.fire({
+                position: 'top',
+                icon: 'success',
+                title: 'המוצר נמחק בהצלחה',
+                showConfirmButton: false,
+                timer: 1500
+              })
+            })
+            .catch((error) => {
+                console.error('Error setting data:', error);
+            });
+              
+            }
+          });
+        }.bind(this), 1200);
+
+      });
+
+      newEl.addEventListener("touchend",function(){
+        clearTimeout(longClickTimer);
+        longClickOccurred = false
+      });
+
+      newEl.addEventListener('contextmenu', function(e) {
+        e.preventDefault();
+      });
+      newEl.addEventListener('selectstart', function(e) {
+        e.preventDefault();
+      });
+      newEl.addEventListener('mousedown', function(e) {
+        e.preventDefault();
+      });
+
+        let div1 = document.createElement("div");
+        div1.textContent =category 
+        var className = "tag " + category
+        div1.className = className;
+        div1.style.display= 'none'
+        newEl.append(div1)
+
         shoppingListEl.append(newEl)
 }
-
-const  inputFieldEl = document.getElementById("input=field")
-const  addButtonEl = document.getElementById("add-button")
-const shoppingListEl = document.getElementById("shopping-list")
-const searchInput = document.getElementById('searchInput');
 
 addButtonEl.addEventListener("click" , function(){
     debugger;
     let inputValue = inputFieldEl.value.trim();
+    if(inputValue == '')return
+    
     var block = false;
     const queryRef = query(shoopingListDB, orderByChild('name'), equalTo(inputValue));
- 
-
-
     (async () => {
         const snapshot = await get(queryRef);
         if (snapshot.exists() ) {
@@ -73,33 +300,121 @@ addButtonEl.addEventListener("click" , function(){
             const childKey = childSnapshot.key;
             const childData = childSnapshot.val();
             console.log(`Key: ${childKey}, Data:`, childData);
-            alert("פריט קיים")
+            Swal.fire({
+              icon: 'error',
+              title: 'אופס...',
+              text: 'פריט קיים במערכת',
+              confirmButtonColor: 'blue',
+            })
           });
         } else {
-            push(shoopingListDB,{name: inputValue , color: "white"} )
-            inputFieldEl.value = ''
+          Swal.fire({
+            title: 'בחר קטגוריה',
+            input: 'select',
+            inputOptions: {
+              'milk': 'מוצרי חלב',
+              'fruits': 'פירות',
+              'veg': 'ירקות',
+              "bread":"מאפים",
+              "meat":"בשר",
+              "clean": "ניקיון",
+              "other":"אחר"
+            },
+            showCancelButton: true,
+            cancelButtonText: 'בטל',
+            confirmButtonText: 'בחר',
+            confirmButtonColor: 'blue',
+            inputValidator: (value) => {
+              return new Promise((resolve) => {
+                if (value !== '') {
+                  resolve();
+                } else {
+                  resolve('Please select an option');
+                }
+              });
+            },
+            preConfirm: () => {
+              // Modify the confirm button style programmatically
+              const confirmButton = Swal.getConfirmButton();
+              confirmButton.style.color = 'red';
+            }
+          }).then((result) => {
+            if (result.isConfirmed) {
+              const selectedOption = result.value;
+              push(shoopingListDB,{name: inputValue , color: "white", category:selectedOption} )
+              inputFieldEl.value = '';
+              Swal.fire({
+                position: 'top',
+                icon: 'success',
+                title: 'המוצר נשמר בהצלחה',
+                showConfirmButton: false,
+                timer: 1500
+              })
+            }
+          });
         }
-      })();
-
-
-  
-   
+      })();  
+      
+     
+     
 })
 searchInput.addEventListener('input', function() {
-    debugger;
+
   const searchQuery = searchInput.value.toLowerCase();
   const listItems = document.querySelectorAll('#shopping-list li');
   for(var i=0;i<listItems.length;i++){
     var item = listItems[i]
     const text = item.textContent.toLowerCase();
-
-    if (text.includes(searchQuery)) {
-      item.style.display = 'block';
+    var display = '';
+    if (text.includes(searchQuery) ) {
+      debugger;
+      if(lastcb =='' || item.className == mapping[lastcb])
+          display = 'block';
+      else display = 'none';
     } else {
-      item.style.display = 'none';
+      display = 'none'
     }
+    item.style.display = display;
   }
 });
+
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function() {
+    // Code to be executed when the button is clicked
+    console.log('Button clicked: ' + this.id);
+    
+    var checked = this.checked;
+    var liList = document.getElementsByTagName('li')
+    for(var i=0;i<liList.length;i++){
+      var li =liList[i]
+      if(checked){
+        lastcb = this.id
+        if(li.className != mapping[this.id])
+          li.style.display = 'none'
+        else li.style.display =  'block'
+      }else{
+        lastcb= ''
+        if(li.className != mapping[this.id])
+          li.style.display =  'block'     
+      }
+    }
+    debugger
+    for (var i = 0; i < buttons.length; i++) {
+        var cbEl = buttons[i];
+        if(cbEl.id != this.id)cbEl.checked = false;
+
+    }
+  });
+}
+
+var mapping = [];
+mapping["milk-box"] = 'li-milk'
+mapping["other-box"] = 'li-other'
+mapping["bread-box"] = 'li-bread'
+mapping["fruits-box"] = 'li-fruits'
+mapping["veg-box"] = 'li-veg'
+mapping["meat-box"] = 'li-meat'
+mapping["clean-box"] = 'li-clean'
 // Custom comparison function to sort by color and then by name
 const customSort = (a, b) => {
     // Sort by color
@@ -120,7 +435,21 @@ const customSort = (a, b) => {
   
     return 0; // Return 0 for equal values
   };
-
   
+  function getCbByCategory(category){
+  if(lastcb =='')return 'block'
+  var mapping = [];
+mapping["milk"] = 'milk-box'
+mapping["other"] = 'other-box'
+mapping["bread"] = 'bread-box'
+mapping["fruits"] = 'fruits-box'
+mapping["veg"] = 'veg-box'
+mapping["meat"] = 'meat-box'
+mapping["clean"] = 'clean-box'
+
+var cbId = mapping[category];
+var checked = document.getElementById(cbId).checked;
+return checked == true ? 'block': 'none'
 
 
+}
